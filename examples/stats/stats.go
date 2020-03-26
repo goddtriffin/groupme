30fb2deda5dfb5c50775e3e81ca45b81470156fd
Todd Everett Griffin <goddtriffin@gmail.com>
2020-03-26 14:20:04 -0400

    Add ability to see who posted the most in the 'stats' example.

--- examples/stats/stats.go
+++ examples/stats/stats.go
@@ -18,6 +18,7 @@ type member struct {
 	PopularityScore int // how often did others upvote them
 	SimpScore       int // how many times did they upvote someone else
 	NarcissistScore int // how many times did they upvote themselves
+	NumMessages     int // how many messages did they send
 }
 
 func newStats(limitTopMessages int) stats {
@@ -29,6 +30,7 @@ func newStats(limitTopMessages int) stats {
 
 func (s *stats) parseMessages(messages []groupme.Message) {
 	for _, message := range messages {
+		s.incNumMessages(message.UserID, message.Name)
 		s.incPopularity(message.UserID, message.Name, len(message.FavoritedBy))
 
 		for _, userID := range message.FavoritedBy {
@@ -52,6 +54,12 @@ func (s *stats) addMember(userID, name string) {
 			m.Name = name
 		}
 	}
+}
+
+func (s *stats) incNumMessages(userID, name string) {
+	s.addMember(userID, name)
+
+	s.Members[userID].NumMessages++
 }
 
 func (s *stats) incPopularity(userID, name string, inc int) {
@@ -82,7 +90,7 @@ func (s *stats) topOfThePops(limit int) []*member {
 	sort.Slice(sorted, func(i, j int) bool { return sorted[i].PopularityScore > sorted[j].PopularityScore })
 
 	top := []*member{}
-	for i := 0; i < limit; i++ {
+	for i := 0; i < limit && i < len(sorted); i++ {
 		top = append(top, sorted[i])
 	}
 
@@ -99,7 +107,7 @@ func (s *stats) topOfTheSimps(limit int) []*member {
 	sort.Slice(sorted, func(i, j int) bool { return sorted[i].SimpScore > sorted[j].SimpScore })
 
 	top := []*member{}
-	for i := 0; i < limit; i++ {
+	for i := 0; i < limit && i < len(sorted); i++ {
 		top = append(top, sorted[i])
 	}
 
@@ -116,7 +124,24 @@ func (s *stats) topOfTheNarcissists(limit int) []*member {
 	sort.Slice(sorted, func(i, j int) bool { return sorted[i].NarcissistScore > sorted[j].NarcissistScore })
 
 	top := []*member{}
-	for i := 0; i < limit; i++ {
+	for i := 0; i < limit && i < len(sorted); i++ {
+		top = append(top, sorted[i])
+	}
+
+	return top
+}
+
+func (s *stats) topPosters(limit int) []*member {
+	sorted := []*member{}
+
+	for _, member := range s.Members {
+		sorted = append(sorted, member)
+	}
+
+	sort.Slice(sorted, func(i, j int) bool { return sorted[i].NumMessages > sorted[j].NumMessages })
+
+	top := []*member{}
+	for i := 0; i < limit && i < len(sorted); i++ {
 		top = append(top, sorted[i])
 	}
 
@@ -126,8 +151,14 @@ func (s *stats) topOfTheNarcissists(limit int) []*member {
 func (s *stats) sprintTopOfThePops(limit int) string {
 	str := "Top of the Pops\n(who has the most upvotes)\n==========\n"
 
-	for i, member := range s.topOfThePops(limit) {
-		str += fmt.Sprintf("%d) %s: %d\n", i+1, member.Name, member.PopularityScore)
+	topPopulars := s.topOfThePops(limit)
+	for i, member := range topPopulars {
+		str += fmt.Sprintf("%d) %s: %d", i+1, member.Name, member.PopularityScore)
+
+		// don't put newline after last ranking
+		if i < len(topPopulars)-1 {
+			str += "\n"
+		}
 	}
 
 	return str
@@ -136,8 +167,14 @@ func (s *stats) sprintTopOfThePops(limit int) string {
 func (s *stats) sprintTopOfTheSimps(limit int) string {
 	str := "Top of the Simps\n(who upvoted other people the most)\n==========\n"
 
-	for i, member := range s.topOfTheSimps(limit) {
-		str += fmt.Sprintf("%d) %s: %d\n", i+1, member.Name, member.SimpScore)
+	topSimps := s.topOfTheSimps(limit)
+	for i, member := range topSimps {
+		str += fmt.Sprintf("%d) %s: %d", i+1, member.Name, member.SimpScore)
+
+		// don't put newline after last ranking
+		if i < len(topSimps)-1 {
+			str += "\n"
+		}
 	}
 
 	return str
@@ -146,8 +183,30 @@ func (s *stats) sprintTopOfTheSimps(limit int) string {
 func (s *stats) sprintTopOfTheNarcissists(limit int) string {
 	str := "Top of the Narcissists\n(who upvoted themselves the most)\n==========\n"
 
-	for i, member := range s.topOfTheNarcissists(limit) {
-		str += fmt.Sprintf("%d) %s: %d\n", i+1, member.Name, member.NarcissistScore)
+	topNarcissists := s.topOfTheNarcissists(limit)
+	for i, member := range topNarcissists {
+		str += fmt.Sprintf("%d) %s: %d", i+1, member.Name, member.NarcissistScore)
+
+		// don't put newline after last ranking
+		if i < len(topNarcissists)-1 {
+			str += "\n"
+		}
+	}
+
+	return str
+}
+
+func (s *stats) sprintTopPoster(limit int) string {
+	str := "Top Poster\n(who posted the most)\n==========\n"
+
+	topPosters := s.topPosters(limit)
+	for i, member := range topPosters {
+		str += fmt.Sprintf("%d) %s: %d", i+1, member.Name, member.NumMessages)
+
+		// don't put newline after last ranking
+		if i < len(topPosters)-1 {
+			str += "\n"
+		}
 	}
 
 	return str
